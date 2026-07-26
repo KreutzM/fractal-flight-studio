c69d64d20b2da579ad1c78cf67e41baaa04b9e2f
Michak <121102425+KreutzM@users.noreply.github.com>
2026-07-26 13:22:42 +0200

    Export offline frame rendering API

--- src/fractal_flight_studio/__init__.py
+++ src/fractal_flight_studio/__init__.py
@@ -3,6 +3,17 @@
 from .camera import CameraState
 from .flight_path import CameraPath, Easing, FlightKeyframe
 from .models import FractalKind, Precision, RenderMode, RenderRequest, Viewport
+from .offline_render import (
+    OfflineFrame,
+    OfflineFrameJob,
+    OfflineFramePlan,
+    OfflineFrameRenderError,
+    OfflineRenderSettings,
+    build_offline_frame_plan,
+    iter_offline_frame_jobs,
+    render_offline_frame,
+    render_offline_frames,
+)
 from .preflight import (
     PreflightIssue,
     PreflightIssueKind,
@@ -20,6 +31,11 @@ __all__ = [
     "Easing",
     "FlightKeyframe",
     "FractalKind",
+    "OfflineFrame",
+    "OfflineFrameJob",
+    "OfflineFramePlan",
+    "OfflineFrameRenderError",
+    "OfflineRenderSettings",
     "Precision",
     "PreflightIssue",
     "PreflightIssueKind",
@@ -30,7 +46,11 @@ __all__ = [
     "RenderMode",
     "RenderRequest",
     "Viewport",
+    "build_offline_frame_plan",
     "build_preflight_plan",
+    "iter_offline_frame_jobs",
+    "render_offline_frame",
+    "render_offline_frames",
     "run_path_preflight",
 ]
 __version__ = "0.9.0"
